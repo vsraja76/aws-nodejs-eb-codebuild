f929011e0e13c487d93b6ec7264d429afecfbcc9
vsraja76 <54915595+vsraja76@users.noreply.github.com>
2019-10-08 13:38:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'BackSpace Academy & Elastic Beanstalk'
+    title: 'Raja AWS practise & Elastic Beanstalk'
     })
     res.status(200).end();
 })
